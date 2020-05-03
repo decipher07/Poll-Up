a94548d5b5e002296720901a3bcbd9bb5482ddcf
decipher07 <social.deej@gmail.com>
2020-05-03 22:02:03 +0530

    Plotting the Results

---
+++ public/main.js
@@ -0,0 +1,61 @@
+const form = document.getElementById('vote-form');
+
+// Form Submit Event
+form.addEventListener('submit', async (e) => {
+    const choice = document.querySelector('input[name=os]:checked').value ;
+
+    const data = {os: choice};
+
+    fetch('http://localhost:3000/poll', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+     .then(res => res.json())
+     .then(data => console.log(data))
+     .catch(err => console.log(err))
+
+    e.preventDefault();
+});
+
+let dataPoints = [
+    {label: 'Ubuntu', y: 0},
+    {label: 'Mint', y: 0},
+    {label: 'Manjaro', y: 0},
+    {label: 'Kali', y: 0},
+    {label: 'Other', y: 0}
+];
+
+const chartContainer = document.querySelector('#chartContainer');
+
+if (chartContainer){
+    const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'theme1',
+        title: {
+            text: 'Distro Results'
+        },
+        data :[
+            {
+                type : 'column',
+                dataPoints: dataPoints
+            }
+        ]
+    });
+    chart.render(); 
+    
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('7b2208005f69a9cba185', {
+          cluster: 'ap2'
+    });
+    
+    var channel = pusher.subscribe('os-poll');
+    channel.bind('os-vote', function(data) {
+          alert(JSON.stringify(data));
+    });
+    
+}
